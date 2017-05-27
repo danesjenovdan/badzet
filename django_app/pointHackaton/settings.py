45bf2c29d1e546e4d44f7ca3c4303a310917496a
T0mTom <juric.tomaz@gmail.com>
2017-05-27 13:48:03 +0200

    static manual

--- django_app/pointHackaton/settings.py
+++ django_app/pointHackaton/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST': 'localhost',
         'NAME': 'badzet',
         'USER': 'muki',
-        'PASSWORD': 'nekadimvecvsakdan',
+        'PASSWORD': 'razvrat',
     }
 }
 
@@ -130,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_manual"),
+]   
