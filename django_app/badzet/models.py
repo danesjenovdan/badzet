d4402b809347224c831863ccaef1cd97e0d62c34
Tomaž Kunst <tomazkunst@gmail.com>
2017-05-27 13:33:13 +0200

    add api

--- django_app/badzet/models.py
+++ django_app/badzet/models.py
@@ -10,11 +10,10 @@ class Timestampable(models.Model):
 
 
 class Budget(Timestampable, models.Model):
-	subject =  models.CharField(max_length=256, blank=True, null=True)
-	konto =  models.IntegerField(max_length=256, blank=True, null=True)
-	revenue_expenses =  models.CharField(max_length=256, blank=True, null=True)
-	classification =  models.CharField(max_length=256, blank=True, null=True)
-	name =  models.CharField(max_length=256, blank=True, null=True)
-	money =  models.FloatField(blank=True, null=True)
-	year =  models.IntegerField(max_length=256, blank=True, null=True)
-	
+    subject = models.CharField(max_length=256, blank=True, null=True)
+    konto = models.IntegerField(max_length=256, blank=True, null=True)
+    revenue_expenses = models.CharField(max_length=256, blank=True, null=True)
+    classification = models.CharField(max_length=256, blank=True, null=True)
+    name = models.CharField(max_length=256, blank=True, null=True)
+    money = models.FloatField(blank=True, null=True)
+    year = models.IntegerField(max_length=256, blank=True, null=True)
--- django_app/badzet/views.py
+++ django_app/badzet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+
+from .models import Budget
 
 # Create your views here.
+FIELDS = ['subject', 'konto', 'revenue_expenses', 'classification', 'name', 'money', 'year']
+
+
+def get_data(request):
+    budgets = Budget.objects.all()
+    data = [model_to_dict(budget,
+                          fields=FIELDS,
+                          exclude=[])
+            for budget in budgets]
+
+    return JsonResponse(data, safe=False)
