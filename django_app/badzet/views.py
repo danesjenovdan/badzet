95e796dab001c399ff4bb0fbcd3b48103e088309
Tomaž Kunst <tomazkunst@gmail.com>
2017-05-27 15:51:07 +0200

    add fitlers

--- django_app/badzet/views.py
+++ django_app/badzet/views.py
@@ -7,16 +7,20 @@ from .models import Budget
 
 # Create your views here.
 FIELDS = ['subject', 'konto', 'revenue_expenses', 'classification', 'name', 'money', 'year']
+TEXT_FIELDS = ['subject', 'name', 'revenue_expenses', 'classification', 'konto']
 
 
 def get_data(request):
-    budgets = Budget.objects.all()
+    budgets = filter_model(request, Budget.objects.all())
     data = [model_to_dict(budget,
                           fields=FIELDS,
                           exclude=[])
             for budget in budgets]
+    classifications = list(set(list(budgets.values_list('classification',
+                                                        flat=True))))
 
-    return JsonResponse(data, safe=False)
+    return JsonResponse({'data': data,
+                         'classifications': classifications}, safe=False)
 
 
 def set_data(request):
@@ -31,3 +35,29 @@ def set_data(request):
         return JsonResponse({"saved": True})
     else:
         return JsonResponse({"saved": False})
+
+
+def filter_model(request, objects):
+    args = {}
+    for field in TEXT_FIELDS:
+        data = request.GET.get(field, None)
+        if data:
+            args[field + '__icontains'] = data
+    objects = objects.filter(*args)
+
+    data = request.GET.get('year', None)
+    if data:
+        years = map(int, data.split(','))
+        objects = objects.filter(year__in=years)
+
+    data = request.GET.get('money', None)
+    if data:
+        money = map(int, data.split(','))
+        objects = objects.filter(money__gte=money[0],
+                                 money__lte=money[0])
+    return objects
+
+
+
+
+
